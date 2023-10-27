d1674496ee84e99df4ffb635065564f0f82a8aa8
whitestorm2346 <andrew770426@gmail.com>
2023-10-27 17:28:28 +0800

    Added two images of main character.

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import '../css/index.css';
 
 function IndexPage(){
     return(
         <>
+        <section className="main">
+            
+        </section>
         </>
     )
 }
