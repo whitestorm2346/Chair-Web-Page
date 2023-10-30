3d38c7a58cd61f174adc3b8b1875ae48773f27d6
whitestorm2346 <andrew770426@gmail.com>
2023-10-31 01:05:35 +0800

    Modified the IndexPage.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import IndexPage from './pages/IndexPage';
--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -3,11 +3,42 @@ import { Link } from "react-router-dom";
 
 import '../css/index.css';
 
+import chair_blue_bg from './images/chair-blue-bg.JPG';
+
 function IndexPage(){
     return(
         <>
-        <section className="main">
-            
+        <section class="main">
+            <div class="main-icon">
+                <img src={chair_blue_bg} alt="chair" />
+            </div>
+            <ul class="main-links">
+                <li class="link-btn">
+                    <Link to={""} class="link-path">
+                        <i class="link-icon fa-brands fa-twitch"></i>
+                    </Link>
+                </li>
+                <li class="link-btn">
+                    <Link to={""} class="link-path">
+                        <i class="link-icon fa-brands fa-instagram"></i>
+                    </Link>
+                </li>
+                <li class="link-btn">
+                    <Link to={""} class="link-path">
+                        <i class="link-icon fa-solid fa-circle-dollar-to-slot"></i>
+                    </Link>
+                </li>
+                <li class="link-btn">
+                    <Link to={""} class="link-path">
+                        <i class="link-icon fa-brands fa-youtube"></i>
+                    </Link>
+                </li>
+                <li class="link-btn">
+                    <Link to={""} class="link-path">
+                        <i class="link-icon fa-solid fa-envelope"></i>
+                    </Link>
+                </li>
+            </ul>
         </section>
         </>
     )
