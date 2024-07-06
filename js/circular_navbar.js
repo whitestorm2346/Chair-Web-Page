61367ac7f62e6976f621edbee092129d2c5772a1
whitestorm2346 <andrew770426@gmail.com>
2024-07-06 18:36:05 +0800

    Finished modifying js code for RWD of circular navbar

--- js/circular_navbar.js
+++ js/circular_navbar.js
@@ -4,25 +4,30 @@ document.addEventListener('DOMContentLoaded', () => {
         const container = document.querySelector('.circular-navbar');
         const containerWidth = container.clientWidth;
         const containerHeight = container.clientHeight;
+
+        const imgContainer = document.querySelector('.img-container')
+        const imgContainerWidth = imgContainer.clientWidth;
+        const imgContainerHeight = imgContainer.clientHeight;
     
         const image = document.querySelector('.character');
         const imageHeight = image.clientHeight;
-        const imageWidth = image.clientWidth
+        const imageWidth = image.clientWidth;
     
-        const radius = imageWidth / 2; 
-        const centerX = containerWidth / 2;
-        const centerY = image.getBoundingClientRect().top + imageHeight / 2;
+        const radius = imgContainerWidth / 2; 
+        const centerX = containerWidth / 2
+        const centerY = containerHeight / 2 + imageHeight / 8;
     
         const angleStep = Math.PI / (links.length - 1);
     
         links.forEach((link, index) => {
             const angle = Math.PI + index * angleStep; 
-            console.log(angle)
+            // console.log(angle)
             const x = centerX + radius * Math.cos(angle);
-            const y = centerY + radius * Math.sin(angle);
-    
+            const y = centerY - radius * Math.sin(angle);
+            // console.log(x, y)
+
             link.style.left = `${x}px`;
-            link.style.bottom = `${y}px`;
+            link.style.top  = `${y}px`;
         });
     }
   
