70ca213316f7e9556da3005320e6378fa9a1c906
whitestorm2346 <andrew770426@gmail.com>
2024-07-08 05:06:40 +0800

    Finished the RWD of main section

--- js/circular_navbar.js
+++ js/circular_navbar.js
@@ -1,35 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
     function positionLinks() {
-        const links = document.querySelectorAll('.main-links .link-btn');
+        const body = document.querySelector('body')
+
         const container = document.querySelector('.circular-navbar');
         const containerWidth = container.clientWidth;
         const containerHeight = container.clientHeight;
-
-        const imgContainer = document.querySelector('.img-container')
-        const imgContainerWidth = imgContainer.clientWidth;
-        const imgContainerHeight = imgContainer.clientHeight;
-    
-        const image = document.querySelector('.character');
-        const imageHeight = image.clientHeight;
-        const imageWidth = image.clientWidth;
-    
-        const radius = imgContainerWidth / 2; 
-        const centerX = containerWidth / 2
-        const centerY = containerHeight / 2 + imageHeight / 8;
-    
-        const angleStep = Math.PI / (links.length - 1);
-
+        
         const mainSection = document.querySelector('.main')
-        mainSection.style.height = `${containerHeight + imgContainerWidth / 2}px`;
+        
+        if(body.clientWidth <= 700) {
+            mainSection.style.height = `${containerHeight}px`;
+        }
+        else {
+            const links = document.querySelectorAll('.main-links .link-btn');
     
-        links.forEach((link, index) => {
-            const angle = Math.PI + index * angleStep; 
-            const x = centerX + radius * Math.cos(angle);
-            const y = centerY - radius * Math.sin(angle);
-
-            link.style.left = `${x}px`;
-            link.style.top  = `${y}px`;
-        });
+            const imgContainer = document.querySelector('.img-container')
+            const imgContainerWidth = imgContainer.clientWidth;
+            const imgContainerHeight = imgContainer.clientHeight;
+        
+            const image = document.querySelector('.character');
+            const imageHeight = image.clientHeight;
+            const imageWidth = image.clientWidth;
+        
+            const radius = imgContainerWidth / 2; 
+            const centerX = containerWidth / 2
+            const centerY = containerHeight / 2 + imageHeight / 8;
+        
+            const angleStep = Math.PI / (links.length - 1);
+    
+            mainSection.style.height = `${containerHeight + imgContainerWidth / 2}px`;
+        
+            links.forEach((link, index) => {
+                const angle = Math.PI + index * angleStep; 
+                const x = centerX + radius * Math.cos(angle);
+                const y = centerY - radius * Math.sin(angle);
+    
+                link.style.left = `${x}px`;
+                link.style.top  = `${y}px`;
+            });
+        }
     }
   
     positionLinks();
